1d0927f9132b396e4e635f1f0ce24cff7340056d
Ubuntu <ubuntu@ip-172-31-32-179.ap-northeast-1.compute.internal>
2023-03-08 13:56:23 +0000

    add clumns

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module CatFind
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+config.generators do |g|
+       g.test_framework :rspec,
+         fixtures: false,
+         view_specs: false,
+         helper_specs: false,
+         routing_specs: false
+         g.factory_bot false
+     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
