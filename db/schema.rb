f43620058dd5cdfa6ee70d50956161528c9cd62c
Ubuntu <ubuntu@ip-172-31-32-179.ap-northeast-1.compute.internal>
2023-03-09 02:36:24 +0000

    error

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_08_063547) do
+ActiveRecord::Schema.define(version: 2023_03_09_013419) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password"
   end
 
 end
