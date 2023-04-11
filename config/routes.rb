38d503bdae32aacbcd3ae729815de3d204220cf3
umeshimaru <amtgptwmjgad@yahoo.co.jp>
2023-04-11 06:08:03 +0000

    セレクトボックス２つ目

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/home', to: 'cats#home'
   get '/search', to: 'cats#search'
   get '/index', to: 'cats#index'
-  get '/show/:id', to: 'cats#show',as: 'show'
+  get '/show/:id', to: 'cats#show',as: :show
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
