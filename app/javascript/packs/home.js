51eb3290dfc1e9c6edd9c0ec64bd58673e958dcb
umekishimaru <riki12344649@gmail.com>
2023-05-04 23:49:59 +0900

    addeventlistner

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,2 +1,17 @@
-console.log(document.getElementById('hello'));
-3
+
+document.addEventListener('DOMContentLoaded', () => {
+ 
+
+  const btn = document.getElementById('button');
+  btn.addEventListener('click', () => {
+  console.log("ボタンが押されました");
+
+});
+ 
+});
+
+
+
+
+
+
