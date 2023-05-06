2917760daf6478ad451a5adadc003f551f192aea
umekishimaru <riki12344649@gmail.com>
2023-05-06 23:39:51 +0900

    edit index.js

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,4 +1,4 @@
-document.addEventListener('turbolinks:load', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('button');
     const form = document.getElementById('button-form');
     if (button && form) {
@@ -12,6 +12,27 @@ document.addEventListener('turbolinks:load', () => {
         }, 5000); // 5000ミリ秒（5秒）後に実行される
       });
     }
+
+    const countBtn = document.getElementById('count-btn');
+ 
+    // HTML要素がクリックされたときにイベント処理を実行する
+    countBtn.addEventListener('click', () => {
+      // テキストボックスに入力された文字列を取得する
+      const text = document.forms.textForm.textBox.value;
+    
+      // 取得した文字列の文字数を出力する
+      console.log(text.length + '文字');
+    
+    });
+    
+    const areaBtn = document.getElementById("area-btn");
+
+    areaBtn.addEventListener("click",() => {
+    const area = document.forms.areaForm.area.value;
+    console.log(area);
+    });
+
+
   });
   
   
