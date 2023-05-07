4b67502ebdb1a345cd96682053516d229e130a29
umekishimaru <riki12344649@gmail.com>
2023-05-07 23:45:25 +0900

    jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+//= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets 
 
