9d25b33f1cca6961ef3dd01740bc3315bd30faab
umekishimaru <riki12344649@gmail.com>
2023-05-10 22:57:27 +0900

    jquery

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,10 +1,16 @@
 
 
     
-  $(function() {
-      const title = $('#title').text();
-      const list = $('.message').text();
-      console.log(title);
-      console.log(list);
-    });
     
+  $(function() {
+      $('input').on('click',function(){
+        console.log('ボタンがクリックされました');
+      });
+
+      $('section > div').on('click',function(){
+        $('section > div').css('background-color', 'blue');
+    })
+  });
+
+
+     
