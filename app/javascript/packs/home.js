b88e8703b8337fca40dafc85058113148269b227
umekishimaru <riki12344649@gmail.com>
2023-05-11 13:13:51 +0900

    jwuery クラス追加

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,3 +1,4 @@
+const { start } = require("@rails/activestorage");
 
 
     
@@ -19,11 +20,28 @@
 
   // });
   $(".blue").on('click',() => {
-    $('section > p').css('color', 'blue');
-  })
+    $('section > p').addClass('sample2');
+  });
   $(".red").on('click',() => {
-    $('section > p').css('color', 'red');
+    $('section > p').addClass('sample');
+  });
+  $(".delete").on('click',() => {
+    $('section > p').removeClass('sample sample2');
+  });
+
+  // hasclassボタンを押すと指定要素にボタンにsampleクラスがついてたらクラスあり、クラスなしを判定する。 ①hasclassクリックされたら以下の処理を行なわれる.②指定要素をbtnという定数に入れる。③ifでcssの設定ありなしパターンを考える
+
+
+  $('.has-class').on('click',() => {
+     const p = $("section > p");
+     if(p.hasClass('sample')) {
+       console.log("スタイルあり");
+     }
+     else {
+       console.log('スタイルなし');
+     }
   })
 
+  
 
 });
