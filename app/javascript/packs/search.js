03bedb11fc014774db8b2373697e1f33275612e6
umekishimaru <riki12344649@gmail.com>
2023-05-24 18:41:07 +0900

    ストロングパラメーター完了

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,25 +1,14 @@
-// $(function() {
+$(function() {
 
   
-//     setTimeout(function () {
-//       $('.alert.alert-danger').remove();
-//     }, 3000);
+    setTimeout(function () {
+      $('.alert.alert-danger').remove();
+    }, 3000);
   
   
 
  
-// });
-
-
-$(function() {
-
-  
-
-  
-    setTimeout(() => {
-      $('.alert.alert-danger').removeClass('.alert alert-danger');}, 3000)
-    
-    
-  
-
 });
+
+
+
---
+++ spec/system/home_spec.rb
@@ -0,0 +1,11 @@
+require'rails_helper' 
+
+RSpec.describe "ホームページ", type: :system do
+  before do
+    driven_by(:rack_test)
+  end  
+
+
+
+
+end 
